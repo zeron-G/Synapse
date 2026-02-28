fed8b105076698de3f1a201e0a521731880d7bc5
zeron <2950243695@qq.com>
2026-02-28 00:53:05 -0500

    chore: add idl/target/ to .gitignore, remove build artifacts

--- idl/target/debug/deps/integration_test-4224280d285865f2.d
+++
@@ -1,5 +0,0 @@
-/mnt/d/data/code/github/Synapse/idl/target/debug/deps/integration_test-4224280d285865f2.d: tests/integration_test.rs
-
-/mnt/d/data/code/github/Synapse/idl/target/debug/deps/integration_test-4224280d285865f2: tests/integration_test.rs
-
-tests/integration_test.rs:
